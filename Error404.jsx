29c0b2a379de8e4c2926a7e6c54d1654d380bc48
Enzo Lacuesta <95934291+EnzoLacuesta@users.noreply.github.com>
2023-06-03 21:55:37 -0300

    Probando cambios
    Cambio de prueba 404

---
+++ Error404.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+
+const Error404 = () => {
+  return (<div>
+              <Nav></Nav>
+              Pepeepepepep
+              <div style={{
+                      'background-color':'#0d4c77',
+                      'height':'90vh',
+                      'display':'grid',
+                      'align-items':'center',
+                      'justify-content':'center'
+                      }}>
+                      <img 
+                        src="https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif" 
+                        alt="" 
+                        style={{'box-shadow':'0 0 20px hsl(287, 42%, 63%)'}} />
+              </div>
+         </div>
+   
+  )
+}
+
+export default Error404
